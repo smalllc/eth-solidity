e5baa7751e523ed5a3b49163bcc298569cf53370
smallcc <18161057239@163.com>
2018-06-25 19:31:53 +0800

    BLC加密货币

---
+++ BLC/contracts/BloggerCoin.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.4;
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract BloggerCoin is StandardToken {
+  string public name = "BloggerCoin";
+  string public symbol = "BLC";
+  uint8 public decimals = 4;
+  uint256 public INITIAL_SUPPLY = 666666;
+  uint256 totalSupply_;
+  function BloggerCoin() {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+  function totalSupply() public view returns (uint256){
+      return totalSupply_;
+  }
+}
